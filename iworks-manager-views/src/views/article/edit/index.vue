53f291422d0a5ea9b46247b70992d21c60dcdad6
如沐清风 <xiaoye08050141@126.com>
2018-09-03 21:24:48 +0800

    heihe

--- iworks-manager-views/src/views/article/edit/index.vue
+++ iworks-manager-views/src/views/article/edit/index.vue
@@ -1,14 +1,16 @@
 <template>
   <el-row :gutter="10">
-    <el-col :span="16"><div class="grid-content bg-purple"></div></el-col>
+    <el-col :span="16">
+      <div class="grid-content bg-purple">
+      
+      </div>
+    </el-col>
     <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
     </el-row>
 </template>
 
 <script>
-export default {
-  name: "demo"
-};
+export default {};
 </script>
 
 <style lang="scss">
