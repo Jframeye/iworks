300f16a4cc5fd05bd821bcff3d57241c0cbd89d4
如沐清风 <xiaoye08050141@126.com>
2018-09-03 18:24:42 +0800

    2018-09-03

---
+++ iworks-manager-views/src/views/article/edit/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <el-row :gutter="10">
+    <el-col :span="16"><div class="grid-content bg-purple"></div></el-col>
+    <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
+    </el-row>
+</template>
+
+<script>
+export default {
+  name: "demo"
+};
+</script>
+
+<style lang="scss">
+</style>
