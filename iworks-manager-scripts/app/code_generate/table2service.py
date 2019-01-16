c4aaf874de51772a141052cec6f51b5ad4c812e5
如沐清风 <xiaoye08050141@126.com>
2019-01-16 12:44:20 +0800

    代码提交

--- iworks-manager-scripts/app/code_generate/table2service.py
+++ iworks-manager-scripts/app/code_generate/table2service.py
@@ -131,11 +131,6 @@ def _generate_service_input4table(db_table, package_name, file_output_path):
     file.write('import com.xiaoye.iworks.api.input.Input;\n')
     file.write('import lombok.Data;\n')
     file.write('import lombok.EqualsAndHashCode;\n')
-    if table_info.hasdecimal:
-        file.write('import java.math.BigDecimal;\n')
-    if table_info.hasdate:
-        file.write('import java.util.Date;\n')
-    file.write('import java.util.List;\n')
 
     file.write('\n/**\n')
     file.write(' * 功能描述: %s 查询入参\n' % table_info.tabledesc)
@@ -159,7 +154,7 @@ def _generate_service_dto4table(db_table, package_name, file_output_path):
     if entity_name.startswith('t_'):
         entity_name = entity_name[2:]
     entity_name = convert(entity_name, '_', True)  # 实体类名称
-    file_name = entity_name + 'QueryInput.java'
+    file_name = entity_name + 'Dto.java'
     file_path = os.path.join(abs_path, '/input', file_name)
     file = open(file_path, 'w', encoding='utf-8')
 
@@ -201,77 +196,22 @@ def _generate_service_constant4table(db_table, package_name, file_output_path):
     if entity_name.startswith('t_'):
         entity_name = entity_name[2:]
     entity_name = convert(entity_name, '_', True)  # 实体类名称
-    file_name = entity_name + 'Service.java'
+    file_name = entity_name + 'Constant.java'
     file_path = os.path.join(abs_path, file_name)
     file = open(file_path, 'w', encoding='utf-8')
 
     table_info = db_table.tableinfo
 
     # package
-    file.write('package %s.api;\r\n' % package_name)
-    # import
-    file.write('import %s.api.dto.%sDto;\n' % (package_name, entity_name))
-    file.write('import %s.api.input.%sQueryInput;\n' % (package_name, entity_name))
-    file.write('import com.xiaoye.iworks.api.result.DataResponse;\n')
-    file.write('import com.xiaoye.iworks.api.result.PageResponse;\n')
+    file.write('package %s.api.constant;\r\n' % package_name)
 
     file.write('\n/**\n')
-    file.write(' * 功能描述: 实体类业务接口【%s】\n' % table_info.tabledesc)
+    file.write(' * 功能描述: 常量类【%s】\n' % table_info.tabledesc)
     file.write(' * @auther: auto create by python \n')
     file.write(' * @date: %s \n' % time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
     file.write(' */\n')
 
-    file.write('public interface %sService {\r\n' % entity_name)
-
-    file.write('    /**\n')
-    file.write('     * 功能描述: 列表查询 <br>\n')
-    file.write('     *     查询条件：\n')
-    file.write('     * @param: queryInput\n')
-    file.write('     * @return: \n')
-    file.write('     * @auther: auto create by python \n')
-    file.write(' * @date: %s \n' % time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
-    file.write(' */\n')
-    file.write('    public PageResponse<%sDto> list%ss(%sQueryInput queryInput);\r\n' % (
-        entity_name, entity_name, entity_name))
-
-    file.write('    /**\n')
-    file.write('     * 功能描述: 明细查询 <br>\n')
-    file.write('     *     查询条件：\n')
-    file.write('     * @param: queryInput\n')
-    file.write('     * @return: \n')
-    file.write('     * @auther: auto create by python \n')
-    file.write(' * @date: %s \n' % time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
-    file.write(' */\n')
-    file.write(
-        '    public DataResponse<%sDto> find%s(%sQueryInput queryInput);\r\n' % (entity_name, entity_name, entity_name))
-
-    file.write('    /**\n')
-    file.write('     * 功能描述: 数据新增 <br>\n')
-    file.write('     * @param: \n')
-    file.write('     * @return: 返回数据主键\n')
-    file.write('     * @auther: auto create by python \n')
-    file.write(' * @date: %s \n' % time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
-    file.write(' */\n')
-    file.write('    public DataResponse<%sDto> insert%s(%sDto dto);\r\n' % (entity_name, entity_name, entity_name))
-
-    file.write('    /**\n')
-    file.write('     * 功能描述: 数据修改 <br>\n')
-    file.write('     * @param: \n')
-    file.write('     * @return: \n')
-    file.write('     * @auther: auto create by python \n')
-    file.write(' * @date: %s \n' % time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
-    file.write(' */\n')
-    file.write('    public DataResponse<Integer> update%s(%sDto dto);\r\n' % (entity_name, entity_name))
-
-    file.write('    /**\n')
-    file.write('     * 功能描述: 数据删除 <br>\n')
-    file.write('     *     查询条件：\n')
-    file.write('     * @param: queryInput\n')
-    file.write('     * @return: \n')
-    file.write('     * @auther: auto create by python \n')
-    file.write(' * @date: %s \n' % time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
-    file.write(' */\n')
-    file.write('    public DataResponse<Integer> delete%s(%sQueryInput queryInput);\r\n' % (entity_name, entity_name))
+    file.write('public class %sConstant {\r\n' % entity_name)
 
     file.write('}')
     file.close()
